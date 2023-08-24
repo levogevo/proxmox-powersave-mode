eb96c754f3c25ffc21975ae96f43c66189a83a67
Levon Gevorgyan <53351550+levogevo@users.noreply.github.com>
2023-08-24 14:30:49 -0500

    add scripts and service

--- enable_powersave.sh
+++ enable_powersave.sh
@@ -2,8 +2,16 @@
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-echo -e "I ${RED}love${NC} Stack Overflow"
 
 AVAILABLE_GOVERNORS=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors)
 echo "Available governors: $AVAILABLE_GOVERNORS"
 
+DESIRED_MODE='powersave'
+if [[ "$AVAILABLE_GOVERNORS" == *"$DESIRED_MODE"* ]]; then
+  echo "$DESIRED_MODE exists, creating service for it..."
+else
+  echo -e "${RED}$DESIRED_MODE mode does not exist, exiting!${NC}"
+  exit 1
+fi
+
+curl -o 
---
+++ set_powersave_mode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
