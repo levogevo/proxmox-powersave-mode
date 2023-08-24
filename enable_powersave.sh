b211b51f1bbadcc0d5b892dbe86445faaada7600
Levon Gevorgyan <53351550+levogevo@users.noreply.github.com>
2023-08-24 14:48:15 -0500

    chmod +x the script

--- enable_powersave.sh
+++ enable_powersave.sh
@@ -17,5 +17,8 @@ fi
 curl -o /opt/set_${DESIRED_MODE}_mode.sh https://raw.githubusercontent.com/levogevo/proxmox-powersave-mode/main/set_${DESIRED_MODE}_mode.sh
 curl -o /etc/systemd/system/$DESIRED_MODE.service https://raw.githubusercontent.com/levogevo/proxmox-powersave-mode/main/${DESIRED_MODE}.service
 
+chmod +x /opt/set_${DESIRED_MODE}_mode.sh
+
 systemctl start ${DESIRED_MODE}.service
 systemctl enable ${DESIRED_MODE}.service
+systemctl status ${DESIRED_MODE}.service
