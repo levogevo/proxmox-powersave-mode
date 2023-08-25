2277ed649e937144107dee51b197e7b690ff2709
Levon Gevorgyan <53351550+levogevo@users.noreply.github.com>
2023-08-25 10:02:51 -0500

    green if powersave exists

--- enable_powersave.sh
+++ enable_powersave.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 AVAILABLE_GOVERNORS=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors)
@@ -8,7 +9,7 @@ echo "Available governors: $AVAILABLE_GOVERNORS"
 
 DESIRED_MODE='powersave'
 if [[ "$AVAILABLE_GOVERNORS" == *"$DESIRED_MODE"* ]]; then
-  echo "$DESIRED_MODE exists, creating service for it..."
+  echo -e "${GREEN}$DESIRED_MODE exists, creating service for it...${NC}"
 else
   echo -e "${RED}$DESIRED_MODE mode does not exist, exiting!${NC}"
   exit 1
